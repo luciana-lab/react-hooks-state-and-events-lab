09384ed352cfb8f423a7264cd161be5144b839c0
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-12 10:58:21 -0500

    commit tree e164ad7ec6457e79ce826e8b286cbbf84253690a

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  // event.target.value will be the value selected by the user
+  function handleFilterChange(e) {
+    setSelectedCategory(e.target.value)
+  }
+
+  // we want to filter the items to only display the ones based on the selected category
+  const filterItems = items.filter(item => {
+    // debugger
+    if (selectedCategory === "All") {
+      return true
+    }
+    return item.category === selectedCategory
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleFilterChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +29,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filterItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
